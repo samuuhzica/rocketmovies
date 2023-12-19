727c171b4e65dbcc011d9665284dc562a1271118
Samuel <samuelfreitasmendes@yahoo.com.br>
2023-12-19 21:42:17 +0000

    first commit

---
+++ rocketmovies/src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+
+import { Button } from "../../components/Button";
+
+export function Details() {
+  return(
+    <Container>
+      <h1>Hello World !</h1>
+      <span>RocketMovies</span>
+      <Button/>
+    </Container>
+  )
+}
---
+++ rocketmovies/src/styles/theme.js
@@ -0,0 +1,20 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#232129",
+    BACKGROUND_850: "#282124",
+    BACKGROUND_800: "#312E38",
+    BACKGROUND_700: "#3E3B47",
+    BACKGROUND_600: "#262519",
+
+    WHITE: "#F4EDE8",
+    PINK: "#FF859B",
+    BLACK: "#0D0C0F",
+
+    GRAY_100: "#999591",
+    GRAY_200: "#948F99",
+    GRAY_300: "#666360",
+    GRAY_900: "#262529",
+
+    RED: "#FF002E"
+  }
+}
